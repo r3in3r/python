11d0d5afbb9641a65cab30a1bd8d296a0a00002b
Rainer Mbongo <rainermbongo@gmail.com>
2022-10-23 22:04:40 +0000

    financial statement

---
+++ statement.py
@@ -0,0 +1,12 @@
+budget =50,000
+marketing= 0.25*50000
+expenses= 0.5*50000
+customerAquisition=.25*50000
+
+print(" Ahmed's Finacial statement")
+print(f"Marketing spend: ", marketing)
+print(f"Expenses spend: ", expenses)
+print(f"Customer Aquisition spend: ", customerAquisition)
+
+customer =customerAquisition/5
+print(f"Ahmed can acquire", customer  )
